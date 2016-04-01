f8c44193d4331125f6769f1efcb6063c27676403
apetri <apetri@phys.columbia.edu>
2016-04-01 17:00:04 -0400

    created MinHeap, MaxHeap for convenience

---
+++ lib/heap.py
@@ -0,0 +1,37 @@
+import Queue
+
+class MinHeap(object):
+	
+	def __init__(self,*args,**kwargs):
+		self._queue = Queue.PriorityQueue(*args,**kwargs)
+
+	def put(self,priority,data):
+		self._queue.put((priority,data))
+
+	def get(self):
+		if self._queue.empty():
+			return None
+		return self._queue.get()
+
+	@property
+	def queue(self):
+		return self._queue.queue
+
+
+class MaxHeap(object):
+	
+	def __init__(self,*args,**kwargs):
+		self._queue = Queue.PriorityQueue(*args,**kwargs)
+
+	def put(self,priority,data):
+		self._queue.put((-priority,data))
+
+	def get(self):
+		if self._queue.empty():
+			return None
+		item = self._queue.get()
+		return (-item[0],item[1])
+
+	@property
+	def queue(self):
+		return [ (-item[0],item[1]) for item in self._queue.queue]
