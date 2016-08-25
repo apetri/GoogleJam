6aed8ab52628468cd6af434be059ad1db18a6636
apetri <apetri@phys.columbia.edu>
2016-08-25 12:20:24 -0400

    my implementation of the heap data structure

--- lib/heap.py
+++ lib/heap.py
@@ -1,45 +1,143 @@
-import Queue
+from __future__ import division
 
-class MinHeap(object):
-	
-	def __init__(self,*args,**kwargs):
-		self._queue = Queue.PriorityQueue(*args,**kwargs)
+from abc import ABCMeta,abstractproperty,abstractmethod
+import numpy as np
 
-	def put(self,priority,data):
-		self._queue.put((priority,data))
+#####################
+#Abstract heap class#
+#####################
 
-	def get(self):
-		if self._queue.empty():
-			return None
-		return self._queue.get()
+class Heap(object):
 
+	__metaclass__ = ABCMeta
+
+	#Heap property comparison method is abstract
+	@abstractmethod
+	def compare(a,b):
+		pass
+
+	#Constructor
+	def __init__(self,maxSize=100):
+		self._queue = np.empty(maxSize,dtype=HeapNode)
+		self._size = 0
+
+	#Heap size
 	@property
-	def queue(self):
-		return self._queue.queue
+	def size(self):
+		return self._size
 
-	@property
-	def empty(self):
-		return self._queue.empty()
-
-
-class MaxHeap(object):
-	
-	def __init__(self,*args,**kwargs):
-		self._queue = Queue.PriorityQueue(*args,**kwargs)
-
-	def put(self,priority,data):
-		self._queue.put((-priority,data))
-
-	def get(self):
-		if self._queue.empty():
-			return None
-		item = self._queue.get()
-		return (-item[0],item[1])
-
-	@property
-	def queue(self):
-		return [ (-item[0],item[1]) for item in self._queue.queue]
-
+	#Empty or not
 	@property
 	def empty(self):
-		return self._queue.empty()
+		return not(self._size)
+
+	#First element 
+	@property 
+	def top(self):
+		if not(self.size):
+			raise ValueError("Heap is empty!!")
+		return self._queue[1].key,self._queue[1].data
+
+	###################
+	#Heap construction#
+	###################
+
+	#Parent 
+	def _parent(self,n):
+		if n==1:
+			return None
+		else:
+			return n//2
+
+	#First child
+	def _child(self,n):
+		return 2*n
+
+	#Bubble up method
+	def _bubble_up(self,n):
+
+		parent = self._parent(n)
+
+		#We are at the top, nothing to do
+		if parent is None:
+			return
+
+		#Recursive bubble up
+		if self.compare(self._queue[n],self._queue[parent]):
+			self._queue[n],self._queue[parent] = self._queue[parent],self._queue[n]
+			self._bubble_up(parent)
+
+	#Bubble down method
+	def _bubble_down(self,n):
+		
+		child = self._child(n)
+		min_index = n
+
+		for i in (0,1):
+			if (child+i<=self.size) and (self.compare(self._queue[child+i],self._queue[min_index])):
+				min_index = child+i
+
+		if min_index!=n:
+			self._queue[min_index],self._queue[n] = self._queue[n],self._queue[min_index]
+			self._bubble_down(min_index)
+	
+	###########
+	#Insertion#
+	###########
+
+	def put(self,key,data=None):
+
+		#Construct node
+		node = HeapNode(key,data)
+
+		#Place in the heap
+		self._size+=1
+		self._queue[self.size] = node
+		self._bubble_up(self.size)
+
+	################# 
+	#Pop top element#
+	#################
+
+	def get(self):
+
+		#If empty return nothing
+		if self.empty:
+			return None
+
+		#Retrieve first element
+		self._queue[1],self._queue[self.size] = self._queue[self.size],self._queue[1]
+		self._size-=1
+		self._bubble_down(1)
+
+		#Return to user
+		node = self._queue[self.size+1]
+		return node.key,node.data 
+
+###########
+#Data node#
+###########
+
+class HeapNode(object):
+
+	def __init__(self,key,data=None):
+		self.key = key
+		self.data = data
+
+
+#############################################################
+#############################################################
+
+######################################
+#MinHeap: smallest element at the top#
+######################################
+
+class MinHeap(Heap):
+	
+	def compare(self,a,b):
+		return a.key<b.key
+
+class MaxHeap(Heap):
+	
+	def compare(self,a,b):
+		return a.key>b.key
